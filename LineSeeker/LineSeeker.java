8d18862b31e03b5ed6fc4cef313db89331c766ab
radosoft <radosoft@gmail.com>
2010-04-18 10:25:02 +0000

    first nearly working version of the line follower! the problem is that B&W is not differentiated properly in my environment on the table

--- LineSeeker/LineSeeker.java
+++ LineSeeker/LineSeeker.java
@@ -13,6 +13,10 @@ import lejos.robotics.navigation.TachoPilot;
  */
 public class LineSeeker {
 
+    // Contants
+    public static final float BW_RATIO = 0.85F;
+    public static final int MOVE_RIGHT_MOD = -1;
+    // Shared variables
     private static SimpleNavigator sn;
     private static boolean onblack = false;
 
@@ -34,28 +38,80 @@ public class LineSeeker {
         sn.forward();
 
         while (!onblack) {
-            // keep forwarding...
+            // Keep forwarding until you reach the line.
         }
-        
-        sn.stop();
 
+        sn.setMoveSpeed(5F);
+        sn.setTurnSpeed(15F);
 
+        // Initialization DONE!
 
-//        for (int i = 0; i < 100; i++) {
-//
-//            System.out.println("OnBlack? " + (onblack ? "YES" : "NO"));
-//
-//
-//            Thread.sleep(200);
-//        }
+        // Variables for optimizing where to search
+        boolean lastSearchRight = false;
+
+        while (true) {
+            while (onblack) {
+                // Keep forwarding! You are doing good
+                sn.forward();
+            }
+
+            // I am not on black, what now?
+
+            // Keep turning right for few degrees, until black or not moving.
+            sn.rotate((lastSearchRight ? 1 : MOVE_RIGHT_MOD) * 15, true);
+            lastSearchRight = !lastSearchRight;
+
+            while (sn.isMoving() && !onblack) {
+                // Keep scanning...
+            }
+
+            if (onblack) {
+                // Yes, I found black! Just stop rotating now.
+                sn.stop();
+
+                // ...and keep forwarding to beyond!
+                sn.forward();
+            } else {
+                // I stopped moving, but I am not on black still.... :(
+
+                // Let me try the other way.
+                sn.rotate((lastSearchRight ? 1 : MOVE_RIGHT_MOD) * 30, true);
+
+                while (sn.isMoving() && !onblack) {
+                    // Keep moving...
+                }
+
+                if (onblack) {
+                    // Yes, I found black! Just stop now
+                    sn.stop();
+
+                    // ...and keep forwarding to beyond!
+                    sn.forward();
+                } else {
+                    // I stopped moving, but I am not on black still and this was a second attempt.
+                    // I failed...
+
+                    System.out.println("ERROR! Path not found (or more than 15 degrees turn found).");
+
+                    while (true != false) {
+                        // Wait until device reset.
+                    }
+                }
+            }
+
+            // Any weird thing that just wont happen to make Java happy.
+            if (sn.getAngle() == -123451) {
+                break;
+            }
+        }
 
         System.out.println("Press a button to exit.");
         Button.waitForPress();
     }
 
     /**
-     * This thread will calibrate (first second) and then will only change bool
-     * value whether I am on back or not.
+     * This thread will calibrate (first 1 second) and then will only change 
+     * boolean value whether sensor/robot is on black line or not.
      */
     static class LightScannerThread extends Thread {
 
@@ -88,11 +144,21 @@ public class LineSeeker {
                     }
                 } else {
                     // Am I on black again?
-                    if (((prevValue + nvalue) / 2) < lightSamplesAvg * 0.9) {
+                    if (((prevValue + nvalue) / 2) < lightSamplesAvg * BW_RATIO) {
+
                         // I am on black
+                        if (!onblack) {
+                            System.out.println("OFF->ON");
+                        }
+
                         onblack = true;
                     } else {
+
                         // I am not on black!!!
+                        if (onblack) {
+                            System.out.println("ON->OFF");
+                        }
+
                         onblack = false;
                     }
                 }
@@ -100,7 +166,7 @@ public class LineSeeker {
                 prevValue = nvalue;
 
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(10);
                 } catch (InterruptedException ex) {
                     // Ok.
                 }
