5e5e01e0942ae8d44dfcc9e84825175c2c707503
radosoft <radosoft@64c6ce96-0c29-09d2-0dec-14ed3af6f5eb>
2010-04-19 19:33:01 +0000

    added distance scanner thread, daemon added distance comparison and sound on finding the ball added more comments

--- trunk/LineSeeker/LineSeeker.java
+++ trunk/LineSeeker/LineSeeker.java
@@ -3,6 +3,8 @@ import lejos.nxt.Button;
 import lejos.nxt.LightSensor;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
+import lejos.nxt.Sound;
+import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.navigation.Pilot;
 import lejos.robotics.navigation.SimpleNavigator;
 import lejos.robotics.navigation.TachoPilot;
@@ -14,32 +16,44 @@ import lejos.robotics.navigation.TachoPilot;
 public class LineSeeker {
 
     // Contants
-    public static final float BW_RATIO = 0.90F;
-    public static final int BW_SAMPLES = 2;
-    public static final int BW_INTERVAL = 10; // in ms
-    public static final int SCAN_RIGHT_MOD = -1;
-    public static final int SCAN_SMALL_DEG = 10;
+    public static final float BW_RATIO = 0.90F; // percentage of white
+    public static final int BW_SAMPLES = 2; // count of samples to be used to compare
+    public static final int BW_INTERVAL = 5; // in ms
+    public static final int DIST_INTERVAL = 50; // distance scanning interval in ms
+    public static final int DIST_TARGET = 15; // satisfactory distance to the ball
+    public static final int SCAN_RIGHT_MOD = -1; // modificator; -1 right, +1 left
+    public static final int SCAN_SMALL_DEG = 20;
     public static final int SCAN_FULL_DEG = 90;
     // Shared variables
-    private static SimpleNavigator sn;
+    private static SimpleNavigator sn; // FIXME: move to private
     private static boolean onblack = false;
+    private static int distance = 255;
     //private static SoundController sound;
 
     public static void main(String[] args) throws Exception {
         System.out.println("Program started.");
 
-        Pilot pilot = new TachoPilot((float) 5.6F, 17, Motor.A, Motor.B, false);
+        Pilot pilot = new TachoPilot((float) 5.6F, 9.4F, Motor.A, Motor.B, false);
         sn = new SimpleNavigator(pilot);
-        sn.setMoveSpeed(5F);
 
-        // Start sound thread
-//        sound = new SoundController();
-//        sound.setDaemon(true);
-//        sound.start();
+        sn.setMoveSpeed(10F);
+        sn.setTurnSpeed(20F);
 
+        // Light scanner thread
         LightScannerThread scannerThread = new LightScannerThread();
         scannerThread.setDaemon(true);
         scannerThread.start();
+
+        // UltraSonic/distance scanner thread
+        DistanceScannerThread distanceThread = new DistanceScannerThread();
+        distanceThread.setDaemon(true);
+        distanceThread.start();
+
+//        TODO: Make thread work.
+//        Start sound thread
+//        sound = new SoundController();
+//        sound.setDaemon(true);
+//        sound.start();
 
         // Wait to calibrate (startup 500, calibration 1000)
         Thread.sleep(1500);
@@ -51,19 +65,41 @@ public class LineSeeker {
             // Keep forwarding until you reach the line.
         }
 
-        sn.setMoveSpeed(10F);
-        sn.setTurnSpeed(10F);
+        // Initialization phase DONE!
 
-        // Initialization DONE!
-
-        // Variables for optimizing where to search
+        // Variables for optimizing search
         boolean lastSearchRight = false;
         boolean fullscan = false;
 
         while (true) {
             while (onblack) {
                 // Keep forwarding! You are doing good
+
+                // FIXME: no need to call forward() so many times again
                 sn.forward();
+
+                // TODO: add steer() logic
+
+                // Is the read ball in sight? Then just make the noise...
+                if (distance < DIST_TARGET) {
+                    // I just reached the target distance!
+                    System.out.println("I just found the ball.");
+
+                    // Make some annoying sound
+                    for (int i = 4; i < 8; i++) {
+                        Sound.playTone(i * 100, 200);
+
+                        try {
+                            Thread.sleep(200);
+                        } catch (InterruptedException ex) {
+                            // OK
+                        }
+                    }
+
+
+                    // TODO: implement logic if the ball is *really* found
+                }
+
             }
 
             // I am not on black, what now?
@@ -133,8 +169,12 @@ public class LineSeeker {
     }
 
     /**
-     * This thread will calibrate (first 1 second) and then will only change 
+     * This thread will wait for the sensor to start (0.5 second), then
+     * calibrate (first 1 second) and then will only change 
      * boolean value whether sensor/robot is on black line or not.
+     *
+     * @author Radoslav Husar
+     * @version 2010-04-19
      */
     static class LightScannerThread extends Thread {
 
@@ -176,6 +216,12 @@ public class LineSeeker {
             float previousAvg = 0;
             int[] previousValues = new int[BW_SAMPLES];
 
+            // inital value is we are on white
+
+            for (int j = 0; j < BW_SAMPLES; j++) {
+                previousValues[j] = (int) lightSamplesAvg;
+            }
+
             while (true) {
 
                 currentValue = ls.readNormalizedValue();
@@ -187,7 +233,8 @@ public class LineSeeker {
                 }
                 previousAvg /= BW_SAMPLES;
 
-                //System.out.println(previousAvg + "/" + currentValue + "/" + lightSamplesAvg);
+                // Uncomment to show the log in format: previous average/current value/sample average
+                // System.out.println(previousAvg + "/" + currentValue + "/" + lightSamplesAvg);
 
                 // Am I on black again?
                 if (previousAvg < (lightSamplesAvg * BW_RATIO)) {
@@ -217,6 +264,34 @@ public class LineSeeker {
 
                 try {
                     Thread.sleep(BW_INTERVAL);
+                } catch (InterruptedException ex) {
+                    // OK
+                }
+            }
+        }
+    }
+
+    /**
+     * This thread will be reporting distance from the UltraSonic sensor
+     * connected to the sensor port S2 and save it in a shared variable for
+     * the main class to access and work with it.
+     *
+     * @author Radoslav Husar
+     * @version 2010-04-20
+     */
+    static class DistanceScannerThread extends Thread {
+
+        @Override
+        public void run() {
+
+            // Get Ultrasonic Sensor at sensor port S2
+            UltrasonicSensor ultraSonicSensor = new UltrasonicSensor(SensorPort.S2);
+
+            while (true) {
+                distance = ultraSonicSensor.getDistance();
+
+                try {
+                    Thread.sleep(DIST_INTERVAL);
                 } catch (InterruptedException ex) {
                     // OK
                 }
